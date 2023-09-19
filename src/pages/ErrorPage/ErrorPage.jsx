274aa4fd50e5dcd83a1c097d92f0de4777149dd7
YuriyAtamanenko <118563472+YuriyAtamanenko@users.noreply.github.com>
2023-09-19 12:55:42 +0300

    add ErrorPage

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -6,7 +6,9 @@ import {
   StyledLink,
   ButtonLink,
   Logo,
+  ContentBox,
 } from './ErrorPage.styled';
+import Icons from './../../images/sprite.svg';
 
 const ErrorPage = () => {
   return (
@@ -14,17 +16,19 @@ const ErrorPage = () => {
       <ErrorContainer>
         <StyledLink to="/">
           <Logo>
-            <use href="../../../src/images/sprite.svg#full-logo"></use>
+            <use href={Icons + '#full-logo'}></use>
           </Logo>
         </StyledLink>
-        <Title>404</Title>
-        <Text>
-          Sorry, you have reached a page that we could not find. It seems that
-          you are lost among the numbers and letters of our virtual space.
-          Perhaps this page went on vacation or decided to disappear into
-          another dimension. We apologize for this inconvenience.
-        </Text>
-        <ButtonLink to="/">Go Home</ButtonLink>
+        <ContentBox>
+          <Title>404</Title>
+          <Text>
+            Sorry, you have reached a page that we could not find. It seems that
+            you are lost among the numbers and letters of our virtual space.
+            Perhaps this page went on vacation or decided to disappear into
+            another dimension. We apologize for this inconvenience.
+          </Text>
+          <ButtonLink to="/">Go Home</ButtonLink>
+        </ContentBox>
       </ErrorContainer>
     </Background>
   );
