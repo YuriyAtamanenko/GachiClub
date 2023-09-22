cb022866ca20d6a92e6f6783eabfea42600434ef
YuriyAtamanenko <118563472+YuriyAtamanenko@users.noreply.github.com>
2023-09-22 21:37:50 +0300

    add redux

--- src/redux/selectors.js
+++
@@ -1,4 +0,0 @@
-export const getTitle = state => state.title;
-export const getCategory = state => state.category;
-export const getToggle = state => state.isToggled;
-export const getIndex = state => state.exerciseIndex;
