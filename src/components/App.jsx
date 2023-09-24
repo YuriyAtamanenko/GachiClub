2df4d63f70cff4153fab5252660a47d1dfc4803e
YuriyAtamanenko <118563472+YuriyAtamanenko@users.noreply.github.com>
2023-09-24 09:59:58 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -92,6 +92,7 @@ function App() {
             <PrivateRoute component={ProfilePage} redirectTo="/signin" />
           }
         />
+        <Route path="*" element={<ErrorPage />} />
       </Route>
       <Route path="*" element={<ErrorPage />} />
     </Routes>
