667a467506a753e9f2076057d371aeb6771ad900
YuriyAtamanenko <118563472+YuriyAtamanenko@users.noreply.github.com>
2023-09-24 09:37:23 +0300

    upd ProductsList

--- src/components/Products/ProductsList/ProductsList.styled.js
+++ src/components/Products/ProductsList/ProductsList.styled.js
@@ -39,3 +39,38 @@ export const Item = styled.ul`
     height: 141px;
   }
 `;
+
+export const DefaultTextWrapper = styled.div`
+  padding-top: 24px;
+
+  @media screen and (min-width: 768px) {
+    padding-top: 100px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    padding-top: 104px;
+  }
+`;
+
+export const DefaultText = styled.p`
+  color: var(--secondary-text-color);
+  font-family: Roboto;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 18px;
+  margin-bottom: 16px;
+
+  @media screen and (min-width: 768px) {
+    font-size: 16px;
+    line-height: 24px;
+    width: 580px;
+  }
+
+  @media screen and (min-width: 1440px) {
+  }
+`;
+
+export const AccentDefaultText = styled.span`
+  color: var(--bright-accent-color);
+`;
