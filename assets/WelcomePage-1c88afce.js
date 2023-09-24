9405659ad7e0b0aacdebf6a7fd875d94c701a5d5
YuriyAtamanenko <118563472+YuriyAtamanenko@users.noreply.github.com>
2023-09-24 13:06:14 +0000

    Deploying to gh-pages from @ YuriyAtamanenko/GachiClub@145c84158aba4dea5d069681a5653e14297fe8de 🚀

--- assets/WelcomePage-1c88afce.js
+++ assets/WelcomePage-1c88afce.js
@@ -1,4 +1,4 @@
-import{n as i,j as e,I as t,N as n}from"./index-7858f4d4.js";import{m as o,a as p,t as d,b as r,d as a,c as s}from"./default-mobile-2x-adbcb5a4.js";const x=i.h1`
+import{n as i,j as e,I as t,N as n}from"./index-8eacac9a.js";import{m as o,a as p,t as d,b as r,d as a,c as s}from"./default-mobile-2x-adbcb5a4.js";const x=i.h1`
   color: #efede8;
   font-size: 38px;
   font-weight: 500;
