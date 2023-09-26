9aa3bd72c317488764009cbff36bbabd60de743b
YuriyAtamanenko <118563472+YuriyAtamanenko@users.noreply.github.com>
2023-09-26 09:33:27 +0300

    Update operations.js

--- src/redux/Profile/operations.js
+++ src/redux/Profile/operations.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-const { token } = JSON.parse(localStorage.getItem('persist:auth'));
+const token = JSON.parse(localStorage.getItem('persist:auth')) || null;
 axios.defaults.baseURL = 'https://fitness-for-all-back-end.onrender.com';
-axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+axios.defaults.headers.common.Authorization = `Bearer ${token.token}`;
 
 export const currenntUserProfile = createAsyncThunk(
   'users/profile',
