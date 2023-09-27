b735984fb1f140d5c04a7c23325d9660d92dcefb
BogdanTitsky <bogdan.titsky@gmail.com>
2023-09-27 21:59:22 +0300

    redux

---
+++ src/redux/Diary/operations.js
@@ -0,0 +1,16 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+axios.defaults.baseURL = 'https://fitness-for-all-back-end.onrender.com';
+
+export const addProductThunk = createAsyncThunk(
+  'dairy/addProduct',
+  async body => {
+    const { productId, calories, amount } = body;
+    const { data } = await axios.post('diary/addProduct', {
+      productId,
+      calories,
+      amount,
+    });
+    return { data };
+  },
+);
---
+++ src/redux/Diary/selectors.js
@@ -0,0 +1,2 @@
+export const selectDiary = state => state.diary;
+export const selectDiaryDate = state => state.diary.date;
