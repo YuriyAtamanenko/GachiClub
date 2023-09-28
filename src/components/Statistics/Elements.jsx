a9da669c92be4514e9e194d938228fa168177bb6
Olia Medvetska <olia.medvetska1967@gmail.com>
2023-09-28 11:46:01 +0300

    add step three

---
+++ src/components/Statistics/Elements.jsx
@@ -0,0 +1,45 @@
+import sprite from '../../images/sprite.svg';
+import {
+  CaloriesContent,
+  CaloriesBack,
+  CaloriesBox,
+  CaloriesText,
+  CaloriesTutorialText,
+  RunIcon,
+  VideoBack,
+  VideoBox,
+  VideoText,
+  VideoIcon,
+  VideoTutorialText,
+} from './Statistics.styled';
+
+export const Elements = () => {
+  return (
+    <>
+      <VideoBox>
+        <VideoBack>
+          <VideoIcon>
+            <use href={sprite + `#icon-play`}></use>
+          </VideoIcon>
+        </VideoBack>
+        <div>
+          <VideoText>350+</VideoText>
+          <VideoTutorialText>Video tutorial</VideoTutorialText>
+        </div>
+      </VideoBox>
+      <CaloriesBox>
+        <CaloriesBack>
+          <RunIcon>
+            <use href={sprite + `#icon-run`}></use>
+          </RunIcon>
+        </CaloriesBack>
+        <CaloriesContent>
+          <CaloriesText>500</CaloriesText>
+          <CaloriesTutorialText>cal</CaloriesTutorialText>
+        </CaloriesContent>
+      </CaloriesBox>
+    </>
+  );
+};
+
+export default Elements;
